aea891a6bbb3c93223af0128f5c0cc3ffe95cee8
Shannon Ding <shannon_ding@yahoo.com>
2020-05-27 02:54:17 -0400

    slider fixes, supercool land slide

--- scripts/slide-it.js
+++ scripts/slide-it.js
@@ -3,4 +3,14 @@ $(document).ready(function(){
   	infinite: true,
   	dots: true
   });
+  $('.land-image-slide').slick({
+  	infinite: true,
+  	dots: false,
+  	autoplay: true,
+  	autoplaySpeed: 4000,
+  	fade: true,
+  	speed:2000,
+  	accessibility: false,
+  	arrows: false
+  });
 });
