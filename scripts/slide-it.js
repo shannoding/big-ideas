cb54ef25228cbcf0728b4fb59cf979e24e6ae243
Shannon Ding <shannon_ding@yahoo.com>
2020-05-27 14:31:58 -0400

    smooth scroll and responsiveness

--- scripts/slide-it.js
+++ scripts/slide-it.js
@@ -13,4 +13,33 @@ $(document).ready(function(){
   	accessibility: false,
   	arrows: false
   });
+
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+  if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+  if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 });
